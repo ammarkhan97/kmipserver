eb1b3d968d5a7452df41abc9e2314a911bbab3a8
unknown <amojo1997@gmail.com>
2021-04-21 00:30:05 -0500

    Init Commit

---
+++ kmipserver/src/main/java/kmip/aws/kmipserver/KmipserverApplication.java
@@ -0,0 +1,13 @@
+package kmip.aws.kmipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmipserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmipserverApplication.class, args);
+	}
+
+}
---
+++ kmipserver/src/test/java/kmip/aws/kmipserver/KmipserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kmip.aws.kmipserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmipserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
